3ec35af512bd73b40c113058e591e589a280cc6d
timvandewalle <tim@vandewalle.mobi>
2023-04-01 22:53:39 +0200

    🎨 Refactor wifi methods into separate file

--- dartscoreduino.ino
+++ dartscoreduino.ino
@@ -23,17 +23,31 @@
 *           
 *
 ********************************************************************************/
-#include <Wire.h> // Enable this line if using Arduino Uno, Mega, etc.
+
+
+/********************************************************************************
+*
+*   Includes
+*
+********************************************************************************/
+#include <Wire.h>  // Enable this line if using Arduino Uno, Mega, etc.
 #include <Adafruit_GFX.h>
 #include "Adafruit_LEDBackpack.h"
 
 #include <SPI.h>
 #include <WiFi.h>
-#include "env.h"
+#include "env.h"      // config for wifi in separate file to keep out of github
 
-char ssid[] = WIFI_SSID;               //  your network SSID (name)
-char pass[] = WIFI_PASSWORD;           // your network password
-int status = WL_IDLE_STATUS;      // the WiFi radio's status
+
+
+/********************************************************************************
+*
+*   Config + data
+*
+********************************************************************************/
+char ssid[] = WIFI_SSID;      //  your network SSID (name)
+char pass[] = WIFI_PASSWORD;  // your network password
+int status = WL_IDLE_STATUS;  // the WiFi radio's status
 
 Adafruit_7segment player1 = Adafruit_7segment();
 Adafruit_7segment player2 = Adafruit_7segment();
@@ -41,96 +55,34 @@ Adafruit_7segment player2 = Adafruit_7segment();
 int scorePlayer1 = 0;
 int scorePlayer2 = 0;
 
+
+
+/********************************************************************************
+*
+*   Setup
+*
+********************************************************************************/
 void setup() {
   Serial.begin(9600);
-  while(!Serial) ;
-  
+  while (!Serial);
+
   Serial.println("DartScore V0.1");
 
   initDisplays();
   initWifi();
 }
 
-void initDisplays(){
-    // set address for 7 segment displays
+void initDisplays() {
+  // set address for 7 segment displays
   player1.begin(0x70);
-  player2.begin(0x71);  
+  player2.begin(0x71);
 }
-
-void initWifi(){
-  printMacAddress();
-  connectToNetwork();
-}
-
-void connectToNetwork(){
-    // check for the presence of the shield:
-  if (WiFi.status() == WL_NO_SHIELD) {
-    Serial.println("WiFi shield not present");
-    // don't continue:
-    while (true);
-  }
-
-  String fv = WiFi.firmwareVersion();
-  if (fv != "1.1.0") {
-    Serial.println("Please upgrade the firmware");
-  }
-
-  // attempt to connect to WiFi network:
-  while (status != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println(ssid);
-    // Connect to WPA/WPA2 network:
-    status = WiFi.begin(ssid, pass);
-
-    // wait 10 seconds for connection:
-    delay(10000);
-  }
-
-   // you're connected now, so print out the data:
-   Serial.print("You're connected to the network");
-   printCurrentNet();
-//   printWifiData();
-}
-
-void printCurrentNet() {
-  // print the SSID of the network you're attached to:
-  Serial.print("SSID: ");
-  Serial.println(WiFi.SSID());
-
-  // print the MAC address of the router you're attached to:
-  byte bssid[6];
-  WiFi.BSSID(bssid);
-  Serial.print("BSSID: ");
-  Serial.print(bssid[5], HEX);
-  Serial.print(":");
-  Serial.print(bssid[4], HEX);
-  Serial.print(":");
-  Serial.print(bssid[3], HEX);
-  Serial.print(":");
-  Serial.print(bssid[2], HEX);
-  Serial.print(":");
-  Serial.print(bssid[1], HEX);
-  Serial.print(":");
-  Serial.println(bssid[0], HEX);
-
-  // print the received signal strength:
-  long rssi = WiFi.RSSI();
-  Serial.print("signal strength (RSSI):");
-  Serial.println(rssi);
-
-  // print the encryption type:
-  byte encryption = WiFi.encryptionType();
-  Serial.print("Encryption Type:");
-  Serial.println(encryption, HEX);
-  Serial.println();
-}
-
 
 void loop() {
   testScore();
 }
 
-void testScore(){
+void testScore() {
   printScore(1, 501);
   printScore(2, 501);
   delay(2000);
@@ -167,29 +119,28 @@ void testScore(){
   delay(5000);
   player1.blinkRate(HT16K33_BLINK_OFF);
   player2.blinkRate(HT16K33_BLINK_OFF);
-
 }
 
-void printScore(int player, int score){
-  if(player == 1){
-    int c = -1;    
-    if(score > scorePlayer1){
+void printScore(int player, int score) {
+  if (player == 1) {
+    int c = -1;
+    if (score > scorePlayer1) {
       c = 1;
     }
 
-    while(score != scorePlayer1){
+    while (score != scorePlayer1) {
       scorePlayer1 = scorePlayer1 + c;
       player1.print(scorePlayer1);
       player1.writeDisplay();
       delay(1);
     }
   } else {
-    int c = -1;    
-    if(score > scorePlayer2){
+    int c = -1;
+    if (score > scorePlayer2) {
       c = 1;
     }
 
-    while(score != scorePlayer2){
+    while (score != scorePlayer2) {
       scorePlayer2 = scorePlayer2 + c;
       player2.print(scorePlayer2);
       player2.writeDisplay();
@@ -198,50 +149,4 @@ void printScore(int player, int score){
   }
 }
 
-void printWifiData() {
-  // print your WiFi shield's IP address:
-  IPAddress ip = WiFi.localIP();
-  Serial.print("IP Address: ");
-  Serial.println(ip);
-  Serial.println(ip);
 
-  // print your MAC address:
-  byte mac[6];
-  WiFi.macAddress(mac);
-  Serial.print("MAC address: ");
-  Serial.print(mac[5], HEX);
-  Serial.print(":");
-  Serial.print(mac[4], HEX);
-  Serial.print(":");
-  Serial.print(mac[3], HEX);
-  Serial.print(":");
-  Serial.print(mac[2], HEX);
-  Serial.print(":");
-  Serial.print(mac[1], HEX);
-  Serial.print(":");
-  Serial.println(mac[0], HEX);
-
-}
-
-void printMacAddress() {
-
-  // the MAC address of your Wifi shield
-
-  byte mac[6];
-
-  // print your MAC address:
-
-  WiFi.macAddress(mac);
-  Serial.print("MAC: ");
-  Serial.print(mac[5],HEX);
-  Serial.print(":");
-  Serial.print(mac[4],HEX);
-  Serial.print(":");
-  Serial.print(mac[3],HEX);
-  Serial.print(":");
-  Serial.print(mac[2],HEX);
-  Serial.print(":");
-  Serial.print(mac[1],HEX);
-  Serial.print(":");
-  Serial.println(mac[0],HEX);
-}
---
+++ wifi_methods.ino
@@ -0,0 +1,83 @@
+void initWifi() 
+{
+  printMacAddress();
+  connectToNetwork();
+}
+
+void printMacAddress() {
+  // the MAC address of your Wifi shield
+  byte mac[6];
+
+  // print your MAC address:
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5], HEX);
+  Serial.print(":");
+  Serial.print(mac[4], HEX);
+  Serial.print(":");
+  Serial.print(mac[3], HEX);
+  Serial.print(":");
+  Serial.print(mac[2], HEX);
+  Serial.print(":");
+  Serial.print(mac[1], HEX);
+  Serial.print(":");
+  Serial.println(mac[0], HEX);
+}
+
+void connectToNetwork() {
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true)
+      ;
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv != "1.1.0") {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+}
+
+void printCurrentNet() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  printMacAddress();
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printWifiData() {
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+  Serial.println(ip);
+}
